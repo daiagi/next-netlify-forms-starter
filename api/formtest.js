d526f5e1f756800c42bc17e2065ae0742ce3f5e7
lukeFunza <luke.fishman@funza.com>
2021-02-07 18:56:52 +0700

    formtet as netlify function

---
+++ api/formtest.js
@@ -0,0 +1,30 @@
+exports.handler = async function(event, context) {
+    // your server-side functionality
+
+    if (event.method === 'POST') {
+        const body = JSON.parse(event.body)
+        // console.log(request);
+        if(body.firstName.includes('f') ) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify(
+                    {
+                        errors: {
+                            firstName: "User name already exists"
+                        }
+                    }
+                )
+            }
+
+    
+        }
+        return  {
+            statusCode: 200,
+            body: 'OK'
+        }
+
+
+    }
+
+    return {statusCode: 405};
+}
